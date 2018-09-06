745e542ea4c0488c9f8fa476d93ac555995f8974
Gabriel Pellegrini <gpellegrinimaciel96@gmail.com>
2018-09-06 09:04:55 -0300

    Starting to implement routes to CRUD

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,33 @@
 // This Module is responsible to handle the route of tasks
 module.exports = app => {
     const Tasks = app.db.models.Tasks;
-    app.get("/tasks", (req, res) => {
-        Tasks.findAll({}).then(tasks => {
-            res.json({ tasks: tasks});
+    app.route("/tasks")
+        .all((req, res) => {
+            //Middleware to pre execution of routes
+        })
+        
+        .get((req, res) => {
+            // "/tasks" : Task listing
+            Tasks.findAll({}).then(tasks => {
+                res.json({tasks: tasks});
+            });
+        })
+
+        .post((req,res) => {
+            // "/tasks": add a new task
+
         });
-    });
+    
+    app.route ("/tasks/:id")
+        .all((req,res) => {
+            // Middleware to pre execution of routes
+        })
+
+        .get((req,res) => {
+            //"tasks/1": update specific task 
+        })
+
+        .delete((req,res) => {
+            //"tasks/1": delete a specific task
+        });
 };
