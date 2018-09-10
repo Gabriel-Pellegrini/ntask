4b2e09e175377b41b0779f71387ad0912f11b444
Gabriel Pellegrini <gpellegrinimaciel96@gmail.com>
2018-09-10 08:58:03 -0300

    Removing id from request bodies and adding FindAllTasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,8 +2,10 @@
 module.exports = app => {
     const Tasks = app.db.models.Tasks;
     app.route("/tasks")
-        .all((req, res) => {
+        .all((req, res,next) => {
             //Middleware to pre execution of routes
+            delete req.body.id
+            next();
         })
         
         .get((req, res) => {
@@ -21,11 +23,18 @@ module.exports = app => {
     app.route ("/tasks/:id")
         .all((req,res) => {
             // Middleware to pre execution of routes
+            delete req.body.id
+            next();
         })
 
         .get((req,res) => {
             //"tasks/1": update specific task 
-        })
+            Tasks.findAll({})
+                .then(results => res.json(results))
+                .catch(error => {
+                    res.status(412).json({msg:error.message});
+                    });
+                })
 
         .delete((req,res) => {
             //"tasks/1": delete a specific task
